a96c8e615af3540584dc6704cc6c804daef5daae
dohaegeily <dohaegeily@gmail.com>
2023-04-24 11:00:34 -0700

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int i = 0;
+while (i <= 9)
+{
+printf("%d", i);
+i++;
+}
+putchar('\n');
+return (0);
+}
