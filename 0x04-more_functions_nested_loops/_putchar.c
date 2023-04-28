3b028ebf0fd67118e3f1e2211c335b6d844693b6
dohaegeily <dohaegeily@gmail.com>
2023-04-27 17:28:50 -0700

    new

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -4,8 +4,8 @@
  * _putchar - writes the character c to stdout
  * @c: the character to print 
  *
- * return on success 1.
- * on error -1. is returned. and errno is set appropriately
+ * Return on success 1.
+ * on error,  -1  is returned. and errno is set appropriately
  */
 int _putchar(char c)
 {
